326b03f2afea086cd1bc5406d8b978cea2ea29a9
Кирильчук Олександр <103460689+Kyrylchuk-of@users.noreply.github.com>
2024-12-06 22:41:21 +0200

    lol2

--- Program.cs
+++ Program.cs
@@ -8,7 +8,6 @@ namespace WinFormsApp2
         [STAThread]
         static void Main()
         {
-
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
